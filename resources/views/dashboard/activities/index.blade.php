34baf90c4a891f572f2550819741121a9cde2e8e
Bonaventure Umolu <devburna@gmail.com>
2021-07-05 16:09:02 +0100

    Update

--- resources/views/dashboard/activities/index.blade.php
+++ resources/views/dashboard/activities/index.blade.php
@@ -8,25 +8,20 @@
                 <h4 class="card-title">Transaction History</h4>
             </div>
             <div class="card-body pt-0">
-                @if(empty(auth()->user()->address()['trnx']))
+            @if(empty(auth()->user()->address()['tx']))
                 <p>All your activities will appear here</p>
                 @else
                 <div class="transaction-table">
                     <div class="table-responsive">
                         <table class="table mb-0 table-responsive-sm">
                             <tbody>
-                                @foreach(auth()->user()->address()['trnx'] as $key => $act)
+                                @foreach(auth()->user()->address()['tx'] as $act)
                                 <tr>
                                     <td>
-                                        {{$act->hash}}
+                                        {{$act['hash']}}
                                     </td>
-                                    @if(!$act->value)
-                                    <td class="text-primary"><a href="https://www.blockchain.com/btc/tx/{{$act->hash}}" target="_blank">Explorer <i class="la la-external-link"></i></a></td>
-                                    @else
-                                    <td class="text-primary">{{$act->value}}</td>
-                                    @endif
+                                    <td class="text-primary">{{$act['input_amount']}} BTC</td>
                                 </tr>
-                                @if($key == 4) @break @endif @endforeach
                             </tbody>
                         </table>
                     </div>
